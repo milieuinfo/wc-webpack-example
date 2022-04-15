e8505ee32330e359d0f83e571bab2620e6e3425d
Lucas Sledsens <lucas_sledsens@hotmail.com>
2022-04-15 10:50:25 +0200

    dynamic view imports

--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@ import { Router } from "@vaadin/router";
 import "uig-webcomponents/lib/components/body";
 import "uig-webcomponents/lib/components/body/styles.css";
 import "./components/template";
-import "./views/home";
-import "./views/video-player-view";
-import "./views/map-view";
 
 const routes = [
   {
     path: "/",
     component: "page-home",
+    action: async () => {
+      await import("./views/home");
+    },
   },
   {
     path: "/video-player",
     component: "video-player-view",
+    action: async () => {
+      await import("./views/video-player-view");
+    },
   },
   {
     path: "/map",
     component: "map-view",
+    action: async () => {
+      await import("./views/map-view");
+    },
   },
 ];
 
