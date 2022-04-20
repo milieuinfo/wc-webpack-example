d8bc7172fc1b5b2804d1a20a384bebf9f004aeb2
Lucas Sledsens <lucas_sledsens@hotmail.com>
2022-04-20 16:19:11 +0200

    update views

--- src/index.js
+++ src/index.js
@@ -4,16 +4,40 @@ import "uig-webcomponents/lib/components/header";
 import "uig-webcomponents/lib/components/footer";
 import "uig-webcomponents/lib/components/grid";
 import "uig-webcomponents/lib/components/body/styles.css";
+<<<<<<< HEAD
 import "uig-webcomponents/lib/components/template";
+=======
+import "./components/template";
+>>>>>>> 373cf77df47b59b103e6b58095197d2f8672de1c
 
 const routes = [
   {
     path: "/",
     component: "page-home",
+    action: async () => {
+      await import("./views/home");
+    },
+  },
+  {
+    path: "/tabs",
+    component: "page-tabs",
+    action: async () => {
+      await import("./views/tabs");
+    },
   },
   {
     path: "/video-player",
     component: "video-player-view",
+    action: async () => {
+      await import("./views/video-player");
+    },
+  },
+  {
+    path: "/map",
+    component: "map-view",
+    action: async () => {
+      await import("./views/map");
+    },
   },
   {
     path: "/upload",
